02349b9e5fcfda072e9c6c291f1b2dd57060c61b
Alexis Rivas <alexis.rivasfernandez@mercadolibre.com>
2019-06-06 13:25:39 -0300

    Initial Commit

---
+++ PrimeraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeraApp
+//
+//  Created by ALEXIS RIVAS FERNANDEZ on 06/06/2019.
+//  Copyright © 2019 ALEXIS RIVAS FERNANDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
